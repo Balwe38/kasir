caad21b3f03305913e8cb5c4b2d5188facb8ff0f
ulyarautama <ulyarautama@gmail.com>
2026-08-05 10:30:49 +0700

    update zuma

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,13 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
-                @if(auth()->user()->role === 'admin')
-                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                    <x-nav-link :href="url('products')" :active="request()->routeIs('products.*')">
-                                        {{ __('Produk') }}
-                                    </x-nav-link>
-                                </div>
-                @endif
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="url('products')" :active="request()->routeIs('products.*')">
+                        {{ __('Produk') }}
+                    </x-nav-link>
+                </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="url('transaksi')" :active="request()->routeIs('transaksi')">
